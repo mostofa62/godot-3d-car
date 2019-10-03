fa7a379269e8e653a93f1cc3f5ed68ffb04db4f5
Golam Mostofa <golammostofa31188@gmail.com>
2019-10-03 22:12:44 +0600

    load scene from resource

--- car.gd
+++ car.gd
@@ -1,14 +1,15 @@
 extends Spatial
 
 var scene=load("res://wheel.tscn")
+
 func _ready():
-	var node = scene.instance()
-	add_child(node)
-	#print(node.transform.basis.x)
-	var basis = Basis()
-	basis.x = Vector3(-2,0,0)
-	basis.z = Vector3(0,0,10)
-	basis.y = Vector3(0,15,0)
-	#basis.scaled(Vector3(0.5,0.085,0.5))
-	node.transform.basis = basis
-	node.transform.scaled(Vector3(0.5,0.085,0.5))
+	#wheel_front_left
+	var wheel_front_left = scene.instance()		
+	add_child(wheel_front_left)
+	wheel_front_left.set_translation(Vector3(-2,0,2))
+	
+	#wheel_front_right
+	var wheel_front_right = scene.instance()		
+	add_child(wheel_front_right)
+	wheel_front_right.set_rotation(Vector3(180,0,0))
+	wheel_front_right.set_translation(Vector3(-2,0,-2))
